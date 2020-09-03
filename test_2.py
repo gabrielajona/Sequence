764ddaeca0910b6a0e7a0ebf1c5ce7ec19b1ca33
gabrielajona <gabbriela16@ru.is>
2020-09-03 16:44:14 +0000

    Defining counter - while n has not reached 0

--- test_2.py
+++ test_2.py
@@ -4,3 +4,8 @@
 # The following sequence starts:; 1, 2, 3, 6, 11, 20, 37,...
 
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+total_n = 0
+
+while n > 0: # Here the input works as a counter 
+  print(total_n)
+  n = n - 1 # Ccounting down - defing the counter

d8c6b7eafd14448e79ca25293cba05a44249111d
gabrielajona <gabbriela16@ru.is>
2020-09-03 16:40:35 +0000

    Algorithm defined - Project description in comments

---
+++ test_2.py
@@ -0,0 +1,6 @@
+# Design an algorithm that takes in an integer
+# that represents the lenght (defined as: n) of a string. 
+# The string should print out n times numbers in a sequence.
+# The following sequence starts:; 1, 2, 3, 6, 11, 20, 37,...
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
